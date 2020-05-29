60ca7ef33123a7137c3a67130b6090af9eef941f
Andrius Kemeris <andrius@inventi.lt>
2020-05-29 20:07:46 +0300

    Paskaita 17: prideta logginimas

--- src/main/java/lt/codeacademy/springmvc/controller/OrderPrivateController.java
+++ src/main/java/lt/codeacademy/springmvc/controller/OrderPrivateController.java
@@ -1,26 +1,30 @@
 package lt.codeacademy.springmvc.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import lt.codeacademy.springmvc.entities.DeliveryInfo;
 import lt.codeacademy.springmvc.entities.Order;
 import lt.codeacademy.springmvc.entities.Product;
 import lt.codeacademy.springmvc.entities.User;
-import lt.codeacademy.springmvc.repositories.UserRepository;
 import lt.codeacademy.springmvc.services.OrderService;
 import lt.codeacademy.springmvc.services.ProductsService;
 import lt.codeacademy.springmvc.services.SpringDataUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/private/orders")
+@Slf4j
 public class OrderPrivateController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderPrivateController.class);
 
     private OrderService orderService;
     private ProductsService productsService;
@@ -43,6 +47,7 @@ public class OrderPrivateController {
         Model model,
         @AuthenticationPrincipal User user
     ) {
+        log.debug("checkout request received, user={}, deliveryInfo={}, productId={}", user, deliveryInfo, productId);
         Product product = productsService.getProduct(productId);
 
         Order order = new Order();
@@ -64,6 +69,7 @@ public class OrderPrivateController {
         Model model,
         @AuthenticationPrincipal User user
     ) {
+        log.debug("checkout request received. user={}, productId={}", user, productId);
         model.addAttribute("deliveryInfo", new DeliveryInfo());
         model.addAttribute("productId", productId);
         model.addAttribute("user", user);
@@ -76,6 +82,7 @@ public class OrderPrivateController {
         @AuthenticationPrincipal User user,
         Model model
     ) {
+        log.debug("showOrderList request received, pageNumber={}, user={}", pageNumber, user);
         Page<Order> userOrders = orderService.getUserOrders(user, pageNumber);
         model.addAttribute("userOrders", userOrders.getContent());
         model.addAttribute("user", user);
