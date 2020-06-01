7676b6c2d922db51edda2146d7f1c15137a720cd
Tadas Simonavičius <tadas@inventi.lt>
2020-06-01 22:12:46 +0300

    Design update.

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,9 +4,9 @@ INSERT INTO Products(title, description, price) VALUES('Plaktukas', 'Puikiai kal
 INSERT INTO Products(title, description, price) VALUES('Vinis', 'Puikiai laiko', 1.50);
 
 INSERT INTO Users(user_id, username, password, name, last_name)
-    VALUES(1, 'user', '{bcrypt}$2y$12$A7x.2lPxE6YdV8ed6OYbDucRiod32wqMF9JNerE.wq4glQWaIjRnO', 'Be', 'Fantazija');
+    VALUES(1, 'user', '{bcrypt}$2y$12$A7x.2lPxE6YdV8ed6OYbDucRiod32wqMF9JNerE.wq4glQWaIjRnO', 'John', 'Doe');
 INSERT INTO Users(user_id, username, password, name, last_name)
-    VALUES(2, 'admin', '{bcrypt}$2y$12$A7x.2lPxE6YdV8ed6OYbDucRiod32wqMF9JNerE.wq4glQWaIjRnO', 'Be', 'Fantazija');
+    VALUES(2, 'admin', '{bcrypt}$2y$12$A7x.2lPxE6YdV8ed6OYbDucRiod32wqMF9JNerE.wq4glQWaIjRnO', 'Jack', 'Sparrow');
 
 INSERT INTO Roles(role_id, role) VALUES(1, 'CUSTOMER');
 INSERT INTO Roles(role_id, role) VALUES(2, 'ADMIN');
@@ -15,4 +15,4 @@ INSERT INTO Users_Roles(user_id, role_id) VALUES(1, 1);
 INSERT INTO Users_Roles(user_id, role_id) VALUES(2, 2);
 
 INSERT INTO Delivery_info(mobile, name, last_name, address, delivery_info_id)
-    VALUES('+37061234567', 'Be', 'Fantazija', 'Lukiskiu 1 g.', 1);
+    VALUES('+37061234567', 'John', 'Doe', 'Lukiskiu 1 g.', 1);
