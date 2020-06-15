9d042f5cfb7ec8b9c3cf76646992d0f8f6a58caa
Andrius Kemeris <andrius@inventi.lt>
2020-06-15 20:14:14 +0300

    Paskaita 26: add crappy validation

--- eshop-rest/eshop-ui/src/pages/ProductForm/ProductForm.js
+++ eshop-rest/eshop-ui/src/pages/ProductForm/ProductForm.js
@@ -2,11 +2,29 @@ import React, { useState } from 'react';
 import productsApi from '../../api/productsApi';
 
 export default () => {
-  const [product, setProduct] = useState();
+  const [product, setProduct] = useState({});
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = event => {
     event.preventDefault();
-    productsApi.createProduct(product);
+    validateProduct(product);
+    if (!errors.title && !errors.description && !errors.price) {
+      productsApi.createProduct(product);
+    }
+  }
+
+  const validateProduct = (product) => {
+      let errors = {}
+      if (!product.price || isNaN(product.price) || product.price <= 0.01) {
+        errors = {...errors,  price: 'Price must be a number bigger than 0.01'}
+      }
+      if (!product.description || product.description === '' || product.description === null) {
+        errors = {...errors,  description: 'Description should not be empty'}
+      }
+      if (!product.title || product.title === '' || product.title === null) {
+        errors = {...errors,  title: 'Title should not be empty'}
+      }
+      setErrors({...errors})
   }
 
   const handleInputChange = (event) => {
@@ -14,20 +32,23 @@ export default () => {
     const name = event.target.name;
     setProduct({ ...product, [name]: value })
   }
-
   return (
+
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="title">Title:</label>
         <input onChange={handleInputChange} type="text" name="title"></input>
+        { errors.title && <div>{errors.title}</div> }
       </div>
       <div>
         <label htmlFor="description">Description:</label>
         <input onChange={handleInputChange} type="text" name="description"></input>
+        { errors.description && <div>{ errors.description }</div>}
       </div>
       <div>
         <label htmlFor="price">Price:</label>
         <input onChange={handleInputChange} type="text" name="price"></input>
+        { errors.price && <div>{ errors.price }</div>}
       </div>
       <div>
         <input type="submit" value="Create"></input>
