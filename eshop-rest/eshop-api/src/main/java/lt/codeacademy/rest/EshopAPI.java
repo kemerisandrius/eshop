d8b0cc7fe91ee3e6ad36c7aab6201a0bcc2f637b
Tadas Simonavičius <tadas@inventi.lt>
2020-06-23 21:15:45 +0300

    Paskaita 31:\n Different DBs by Spring Boot profile

--- eshop-rest/eshop-api/src/main/java/lt/codeacademy/rest/EshopAPI.java
+++ eshop-rest/eshop-api/src/main/java/lt/codeacademy/rest/EshopAPI.java
@@ -1,25 +1,13 @@
 package lt.codeacademy.rest;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EshopAPI implements CommandLineRunner {
-
-	private final MessageBean messageBean;
-
-	public EshopAPI(MessageBean messageBean) {
-		this.messageBean = messageBean;
-	}
+public class EshopAPI {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EshopAPI.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(messageBean.getMessage());
-	}
 }
