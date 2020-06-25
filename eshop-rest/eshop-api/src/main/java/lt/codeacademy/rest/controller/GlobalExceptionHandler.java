c95f909cb580540b587eaafa7ff522fc281b7cc4
Andrius Kemeris <andrius@inventi.lt>
2020-06-25 19:24:47 +0300

    Paskaita 32: added request param missing exception

--- eshop-rest/eshop-api/src/main/java/lt/codeacademy/rest/controller/GlobalExceptionHandler.java
+++ eshop-rest/eshop-api/src/main/java/lt/codeacademy/rest/controller/GlobalExceptionHandler.java
@@ -3,21 +3,35 @@ package lt.codeacademy.rest.controller;
 import lt.codeacademy.rest.services.exceptions.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({RuntimeException.class, ProductNotFoundException.class})
-    public ResponseEntity<ErrorContext> handleErrors(Exception ex) {
+    @ExceptionHandler({RuntimeException.class, ProductNotFoundException.class, MissingServletRequestParameterException.class})
+    public ResponseEntity<ErrorContext> handleErrors(Exception ex, WebRequest request) {
         if (ex instanceof ProductNotFoundException) {
             HttpStatus status = HttpStatus.NOT_FOUND;
             return handleProductNotFoundException((ProductNotFoundException) ex, status);
-        } else {
+        } else if (ex instanceof MissingServletRequestParameterException) {
+            HttpStatus status = HttpStatus.BAD_REQUEST;
+            return handleMissingParameterException((MissingServletRequestParameterException) ex, status);
+        }  else {
             HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
             return handleRuntimeException((RuntimeException) ex, status);
         }
+    }
+
+    private ResponseEntity<ErrorContext> handleMissingParameterException(
+            MissingServletRequestParameterException ex, HttpStatus status
+    ) {
+        ErrorContext errorContext = new ErrorContext();
+        errorContext.code = "3";
+        errorContext.error = ex.getMessage();
+        return new ResponseEntity<>(errorContext, status);
     }
 
     private ResponseEntity<ErrorContext> handleRuntimeException(RuntimeException ex, HttpStatus status) {
