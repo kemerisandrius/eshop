ee7593e0dea9211b0daeea24395a046fa8c46ec2
Tadas Simonavičius <tadas@inventi.lt>
2020-07-01 20:38:40 +0300

    Paskaita 35:
    Axios interceptors

--- eshop-rest/eshop-ui/src/api/index.js
+++ eshop-rest/eshop-ui/src/api/index.js
@@ -1,15 +1,18 @@
 import axios from 'axios'
 
-let credentials = {}
+let credentials = null
 
 const HTTP = axios.create({
     baseURL: 'http://localhost:8080',
     headers: {
         'X-Requested-With': 'XMLHttpRequest'
-    },
-    auth: credentials
+    }
+})
+
+HTTP.interceptors.request.use(config => {
+    config.auth = credentials
+    return config
 })
 
 export const setCredentials = c => credentials = c
-
 export { HTTP as default }
